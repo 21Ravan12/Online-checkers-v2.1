abc65e9bc782e324096efa515cf8ef250db1ff14
Ravan Asgarov <asgarovravan@gmail.com>
2025-07-15 22:29:44 +0400

    Update router.js

--- public/js/router.js
+++ public/js/router.js
@@ -1,38 +1,68 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const pages = ["menu", "offlineMenu", "onlineMenu", "quickMatchMenu", "gameListMenu", "createGameMenu"
-    , "settingsMenu", "offlineGameContainer", "onlineGameContainer", "waitingScreen", "waitingScreenForAuth", "loginMenu", "signupMenu"
-    , "sendCodeMenu", "verifyMenuForRefreshPassword", "refreshPasswordMenu", "verifyMenuForSignup", "AccountMenu"];
+    const pages = ["menu", "offlineMenu", "onlineMenu", "quickMatchMenu", "gameListMenu", "createGameMenu", 
+    "settingsMenu", "offlineGameContainer", "onlineGameContainer", "waitingScreen", "waitingScreenForAuth"
+    , "loginMenu", "signupMenu", "sendCodeMenu", "verifyMenuForRefreshPassword", "refreshPasswordMenu",
+    "verifyMenuForSignup", "AccountMenu", "PersonalAccountMenu", "gameHistoryList", "reviewGameContainer",
+    "afterEndOfOnlineGameContainer"];
     
     function navigateTo(page) {
         pages.forEach(p => {
             document.getElementById(`${p}`).style.display = (p === page) ? "block" : "none";
         });
+        sessionStorage.setItem("currentPage", page);
         defoultActions(page);
     }
     function defoultActions(page) {
+        if (page === 'menu') {
+            if (socket && !socket.disconnected) {
+                socket.disconnect();
+            }            
+        }
         if (page === 'gameListMenu' || page === 'quickMatchMenu') {
             refreshGameList(); 
         }
+        if (page === 'gameHistoryList') {
+            getGameHistoryList(); 
+        }
         if (page === 'quickMatchMenu' || page === 'gameListMenu' || page === 'createGameMenu' || page === 'onlineMenu') {
-            leaveRoom(); 
+            if (activeRoomId) {
+                leaveRoom(); 
+            }
+        }
+        if (page === 'quickMatchMenu' || page === 'gameListMenu' || page === 'createGameMenu') {
+            if (!socket || socket.disconnected) {
+                alert("Socket is disconnected, initializing..."); 
+                navigateTo('onlineMenu');
+            }
         }
         if (page === 'onlineMenu') {
-            if(!userDefined){
+            if (!userDefined) {
                 alert('Please log in before accessing the online mode.');
                 navigateTo('menu');
+                return;
+            }
+
+            if (!socket || socket.disconnected) {
+                initSocket();
             }
         }
         if (page === 'offlineGameContainer') {
             document.getElementById('offlineGameContainer').style.display = "flex";
             playTwoPlayer(); 
         }
+        if (page === 'reviewGameContainer') {
+            document.getElementById('reviewGameContainer').style.display = "flex";
+        }
         if (page === 'onlineGameContainer') {
             document.getElementById('onlineGameContainer').style.display = "flex";
         }
 
     }
-
-    navigateTo("menu");
+    if (sessionStorage.getItem("currentPage")) {
+        navigateTo(sessionStorage.getItem("currentPage"));
+    } else {
+        navigateTo("menu");
+    }
 
     window.navigateTo = navigateTo;
 });
